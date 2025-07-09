428dfc7b82ffc6fe48329c97f0308663ec1604d5
ujaRHR <rhraju.dev@gmail.com>
2025-07-09 14:36:27 +0600

    Typescript: binary search & ...

--- typescript/lower_bound.ts
+++ typescript/lower_bound.ts
@@ -1,17 +1,17 @@
 // Binary Search (Lower Bound)
 // Find the first index where element >= target.
 
-const arr = [
+const arr: number[] = [
   5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
   100,
 ];
 
-function lowerBound(arr, target) {
-  let low = 0;
-  let high = arr.length;
+function lowerBound(arr: number[], target: number): number {
+ let low: number = 0;
+ let high: number = arr.length;
 
   while (low < high) {
-    let mid = Math.floor((low + high) / 2);
+    let mid: number = Math.floor((low + high) / 2);
 
     if (arr[mid] < target) low = mid + 1;
     else high = mid;
@@ -21,5 +21,5 @@ function lowerBound(arr, target) {
 }
 
 // Implementation
-const result = lowerBound(arr, 70);
+const result: number = lowerBound(arr, 70);
 console.log(result);
