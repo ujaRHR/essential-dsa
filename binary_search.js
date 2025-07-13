4392702dac6b5cd8a9557d3dd6c7b852cef2650f
ujaRHR <rhraju.dev@gmail.com>
2025-07-13 22:46:45 +0600

    formatting...

--- binary_search.js
+++ binary_search.js
@@ -2,8 +2,7 @@
 // Find the exact index of the target
 
 const arr = [
-  5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
-  100,
+  5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100
 ];
 
 function binarySearch(arr, target) {
